60313c78c9357efb8652e3d706efa6cab701a309
Elian Frentzel <elian.y.frentzel@gmail.com>
2025-04-11 15:12:33 +0200

    parallelise execution

--- main.py
+++ main.py
@@ -9,43 +9,61 @@ def get_jobs() -> list[str]:
         jobs = fd.read().split("\n")
     return jobs
 
-def run(job: str):
-    database_path = pathlib.Path("storage", "database.json")
-    stderr_path = pathlib.Path("storage", "stderr")
-    stdout_path = pathlib.Path("storage", "stdout")
+def init_pool_processes(the_lock):
+    global lock
+    lock = the_lock
 
-    result = subprocess.run(shlex.split(job), stdout=subprocess.PIPE)
-    
-    with open(database_path) as fd:
-        database = json.load(fd)
-    if job in database.values():
-        return
-
-    if len(database) == 0:
-        index = 0
-    else:
-        index = max([int(i) for i in database.keys()]) + 1
-
-    database[str(index)] = job
-    with open(database_path, "w") as fd:
-        database = json.dump(database, fd, indent=4)
-
-    if result.stdout != None:
-        stdout_path.joinpath(str(index)).write_bytes(result.stdout)
-    if result.stderr != None:
-        stderr_path.joinpath(str(index)).write_bytes(result.stderr)
-
-def main(serial:bool):
-
-    if serial:
-        procs = 1
-    else:
-        procs = 2
+class Runner:
+    def run(self, job, is_locked: bool = True):
+        database_path = pathlib.Path("storage", "database.json")
+        stderr_path = pathlib.Path("storage", "stderr")
+        stdout_path = pathlib.Path("storage", "stdout")
         
-    # with multiprocessing.Pool(procs) as pool:
-    #     pool.map(run, get_jobs())
-    for job in get_jobs():
-        run(job)
+        if is_locked: lock.acquire()
+        with open(database_path) as fd:
+            if job in json.load(fd).values():
+                if is_locked: lock.release()
+                return
+        if is_locked: lock.release()
+
+        result = subprocess.run(shlex.split(job), stdout=subprocess.PIPE)
+
+        if is_locked: lock.acquire()
+
+        with open(database_path) as fd:
+            database = json.load(fd)
+            
+        if len(database) == 0:
+            index = 0
+        else:
+            index = max([int(i) for i in database.keys()]) + 1
+
+        if result.stdout != None:
+            stdout_path.joinpath(str(index)).write_bytes(result.stdout)
+        if result.stderr != None:
+            stderr_path.joinpath(str(index)).write_bytes(result.stderr)
+
+        database[str(index)] = job
+        with open(database_path, "w") as fd:
+            json.dump(database, fd, indent=4)
+        
+        if is_locked: lock.release()
+    
+    def start(self, serial: bool):
+        if serial:
+            for job in get_jobs():
+                self.run(job, False)
+        
+        else:
+            lock = multiprocessing.Lock()
+            pool = multiprocessing.Pool(initializer=init_pool_processes, initargs=(lock,))
+            pool.map(self.run, get_jobs())
+            pool.close()
+            pool.join()
+
+def main():
+    r = Runner()
+    r.start(False)
 
 if __name__ == '__main__':
-    main(True)
+    main()
